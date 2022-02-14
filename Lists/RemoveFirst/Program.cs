06e181b365c31db9389a0f72d2188ae941409148
yoanamilcheva <j.ivanova2018@pmg-pernik.com>
2022-02-14 09:34:51 +0200

    add RemoveFirst project

---
+++ Lists/RemoveFirst/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RemoveFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int first = nums[0];
+            while(nums.Contains(first))
+            {
+                nums.Remove(first);
+            }
+
+            //nums.RemoveAll(x=>x == first);
+
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+
+      
+    }
+}
